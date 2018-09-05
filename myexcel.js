8af12347d4c706f94d6b1145ec5ae3cdc444e56d
ErikvdBurgwal <erikvandeburgwal@gmail.com>
2018-09-05 14:10:46 +0200

    Conditional Formatting
    Added Confitional formatting. 
    
    example usage: see sample.html
        excel.addConditionalFormatting(0, 4, 'iconSet');		
        excel.addConditionalFormatting(0, 5, 'iconSet', '5Quarters', true, [{ type: 'num', val: '0' }, { type: 'num', val: '200' }, { type: 'num', val: '400' }, { type: 'num', val: '600' }, { type: 'num', val: '800' }]);		
            
    
    nb, my editor [Visual Studio] applied some formatting to the javascript-file which caused quite some additional changes in the file that are non-functional.

--- myexcel.js
+++ myexcel.js
@@ -22,23 +22,23 @@ $JExcel = {
         return (componentToHex(r) + componentToHex(g) + componentToHex(b)).toUpperCase();
     }
 
-	$JExcel.toExcelUTCTime = function (date1){
-		var d2=Math.floor(date1.getTime()/1000);													// Number of seconds since JS epoch
-		d2=Math.floor(d2/86400)+25569;																// Days since epoch plus difference in days between Excel EPOCH and JS epoch
-		
-		var seconds = date1.getUTCSeconds()+60*date1.getUTCMinutes()+60*60*date1.getUTCHours();		// Number of seconds of received hour
-		var SECS_DAY= 60 * 60 * 24;																	// Number of seconds of a day
-		return d2+(seconds/SECS_DAY);																// Returns a local time !!
-	}
+    $JExcel.toExcelUTCTime = function (date1) {
+        var d2 = Math.floor(date1.getTime() / 1000);													// Number of seconds since JS epoch
+        d2 = Math.floor(d2 / 86400) + 25569;																// Days since epoch plus difference in days between Excel EPOCH and JS epoch
 
-	$JExcel.toExcelLocalTime = function (date1){
-		var d2=Math.floor(date1.getTime()/1000);													// Number of seconds since JS epoch
-		d2=Math.floor(d2/86400)+25569;																// Days since epoch plus difference in days between Excel EPOCH and JS epoch
-		var seconds = date1.getUTCSeconds()+60*date1.getUTCMinutes()+60*60*date1.getUTCHours();		// Number of seconds of received hour
-		seconds = seconds-60*(date1.getTimezoneOffset());											// Differences in seconds between UTC and LOCAL this depends on date becase daylight saving time
-		var SECS_DAY= 60 * 60 * 24;																	// Number of seconds of a day
-		return d2+(seconds/SECS_DAY);																// Returns a local time !!
-	}
+        var seconds = date1.getUTCSeconds() + 60 * date1.getUTCMinutes() + 60 * 60 * date1.getUTCHours();		// Number of seconds of received hour
+        var SECS_DAY = 60 * 60 * 24;																	// Number of seconds of a day
+        return d2 + (seconds / SECS_DAY);																// Returns a local time !!
+    }
+
+    $JExcel.toExcelLocalTime = function (date1) {
+        var d2 = Math.floor(date1.getTime() / 1000);													// Number of seconds since JS epoch
+        d2 = Math.floor(d2 / 86400) + 25569;																// Days since epoch plus difference in days between Excel EPOCH and JS epoch
+        var seconds = date1.getUTCSeconds() + 60 * date1.getUTCMinutes() + 60 * 60 * date1.getUTCHours();		// Number of seconds of received hour
+        seconds = seconds - 60 * (date1.getTimezoneOffset());											// Differences in seconds between UTC and LOCAL this depends on date becase daylight saving time
+        var SECS_DAY = 60 * 60 * 24;																	// Number of seconds of a day
+        return d2 + (seconds / SECS_DAY);																// Returns a local time !!
+    }
 
     // For styles see page 2127-2143 of the standard at
     // http://www.ecma-international.org/news/TC45_current_work/Office%20Open%20XML%20Part%204%20-%20Markup%20Language%20Reference.pdf
@@ -112,7 +112,7 @@ $JExcel = {
         'xmlns:x14="http://schemas.microsoft.com/office/spreadsheetml/2009/9/main" xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac" ' +
         'xmlns:xm="http://schemas.microsoft.com/office/excel/2006/main">' +
         '{views}{columns}' +
-        '<sheetData>{rows}</sheetData>{mergeCells}</worksheet>';
+        '<sheetData>{rows}</sheetData>{mergeCells}{conditionalFormattings}</worksheet>';
 
 
     // --------------------- BEGIN of generic UTILS
@@ -134,7 +134,7 @@ $JExcel = {
     }
 
     function pushI(list, value) {
-        list.push(value); 
+        list.push(value);
         return list.length - 1;
     }
 
@@ -159,9 +159,10 @@ $JExcel = {
 
     function getAsXml(sheet) {
         return templateSheet.replace('{views}', generateViews(sheet.views))
-                            .replace('{columns}', generateColums(sheet.columns))
-                            .replace("{rows}", generateRows(sheet.rows, sheet.mergeCells))
-                            .replace("{mergeCells}", generateMergeCells(sheet.mergeCells));
+            .replace('{columns}', generateColums(sheet.columns))
+            .replace("{rows}", generateRows(sheet.rows, sheet.mergeCells))
+            .replace("{mergeCells}", generateMergeCells(sheet.mergeCells))
+            .replace("{conditionalFormattings}", generateConditionalFormattings(sheet.conditionalFormattings));
     }
 
 
@@ -207,6 +208,37 @@ $JExcel = {
         var view = { panes: [pane] };
         view.workbookViewId = pushI(this.views, view);
     }
+
+    function addConditionalFormatting(ruleType, column, iconSet, showValue, cfvos) {
+        var colName = cellNameH(column);                //eg: 5 -> F
+        var range = colName + ':' + colName;            //eg: F:F
+        var cf = null;
+
+        for (var i = 0; i < this.conditionalFormattings.length - 1; i++) { 
+            if (this.conditionalFormattings[i].range == range) {            //Look for an existing conditional formatting for the same column
+                cf = this.conditionalFormattings[i];
+                break;
+            }
+        }
+
+        if (!cf) {
+            cf = { range: range, rules: [] };                               // Create new cf if not existing and
+            pushI(this.conditionalFormattings, cf);                         // add to sheet.
+        }        
+
+        var rule = {
+            type: ruleType || 'iconSet',
+            iconSet: iconSet || '5Quarters',
+            showValue: showValue ? true : false                            // convert any value to true/false
+        };
+
+        if (!cfvos || !cfvos.length) {
+            cfvos = [{ type: 'num', val: '0' }, { type: 'num', val: '1' }, { type: 'num', val: '1' }, { type: 'num', val: '1' }, { type: 'num', val: '1' }];
+        }
+        rule.cfvos = cfvos;
+
+        cf.rules.push(rule);
+    }
     // ------------------- END Sheet DATA Handling
 
 
@@ -214,7 +246,19 @@ $JExcel = {
         var oSheets = {
             sheets: [],
             add: function (name) {
-                var sheet = { id: this.sheets.length + 1, rId: "rId" + (3 + this.sheets.length), name: name, rows: [], columns: [], getColumn: getColumn, set: setSheet, getRow: getRow, getCell: getCell, mergeCells: [], views: [], freezePane: freezePane };
+                var sheet = {
+                    id: this.sheets.length + 1,
+                    rId: "rId" + (3 + this.sheets.length),
+                    name: name,
+                    rows: [], columns: [],
+                    getColumn: getColumn,
+                    set: setSheet,
+                    getRow: getRow,
+                    getCell: getCell,
+                    mergeCells: [], views: [], conditionalFormattings: [],
+                    freezePane: freezePane,
+                    addConditionalFormatting: addConditionalFormatting
+                };
                 return pushI(this.sheets, sheet);
             },
             get: function (index) {
@@ -329,12 +373,12 @@ $JExcel = {
         if (style.align) {
             var h = align[style.align.charAt(0)];
             var v = align[style.align.charAt(1)];
-	        var w = align[style.align.charAt(2)];
+            var w = align[style.align.charAt(2)];
             if (h || v || w) {
                 alignXml = "<alignment ";
                 if (h) alignXml = alignXml + ' horizontal="' + h + '" ';
                 if (v) alignXml = alignXml + ' vertical="' + v + '" ';
-				if (w) alignXml = alignXml + ' ' + w + '="1" ';
+                if (w) alignXml = alignXml + ' ' + w + '="1" ';
                 alignXml = alignXml + " />";
             }
         }
@@ -344,7 +388,7 @@ $JExcel = {
         if ((style.fill || 0) != 0) s = s + ' applyFill="1" ';
         if ((alignXml || "") != "") s = s + ' applyAlignment="1" ';
         s = s + '>';
-        s = s + alignXml; 
+        s = s + alignXml;
         return s + "</xf>";
     }
 
@@ -406,22 +450,22 @@ $JExcel = {
 
         var oStyles = {
             add: function (a) {
-                var style = {isstring: a.isstring};
+                var style = { isstring: a.isstring };
                 if (a.fill && a.fill.charAt(0) == "#") style.fill = 2 + findOrAdd(fills, a.fill.toString().substring(1).toUpperCase());                  // If there is a fill color add it, with a gap of 2, because of the TWO DEFAULT HARDCODED fills
                 if (a.font) style.font = findOrAdd(fonts, normalizeFont(a.font.toString().trim()));
                 if (a.format) style.format = findOrAdd(formats, a.format);
                 if (a.align) style.align = normalizeAlign(a.align);
                 if (a.border) style.border = 1 + findOrAdd(borders, normalizeBorders(a.border.toString().trim()));                                          // There is a HARDCODED border         
 
-                createKey(style);   
+                createKey(style);
                 for (var i = styles.length - 1; i >= 0; i--) {
                     if (styles[i].key == style.key) return 1 + i;
-                }  
+                }
                 return 1 + pushI(styles, style);                                                            // Add the style and return INDEX+1 because of the DEFAULT HARDCODED style
             }
         };
 
-        if (!defaultFont) defaultFont="Calibri Light 12 0000EE";
+        if (!defaultFont) defaultFont = "Calibri Light 12 0000EE";
         oStyles.add({ font: defaultFont });
 
 
@@ -429,8 +473,8 @@ $JExcel = {
             createKey(thisOne);
 
             for (var i = styles.length - 1; i >= 0; i--) {
-                 if (styles[i].key == thisOne.key) return i;
-            } 
+                if (styles[i].key == thisOne.key) return i;
+            }
             return pushI(styles, thisOne);
         }
 
@@ -439,7 +483,7 @@ $JExcel = {
         }
         oStyles.toStyleSheet = function () {
             var s = '<?xml version="1.0" encoding="utf-8"?><styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" ' +
-                    'xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" mc:Ignorable="x14ac" xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac">';
+                'xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" mc:Ignorable="x14ac" xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac">';
 
             s = s + '<numFmts count="' + (formats.length - baseFormats) + '">';
             for (var i = baseFormats; i < formats.length; i++) s = s + '<numFmt numFmtId="' + (i) + '" formatCode="' + formats[i] + '"/>';
@@ -502,35 +546,35 @@ $JExcel = {
         if (typeof string != 'string') string = null ? '' : (string + '');
 
         return (string && reHasUnescapedHtml.test(string))
-          ? string.replace(reUnescapedHtml, escapeHtmlChar)
-          : string;
+            ? string.replace(reUnescapedHtml, escapeHtmlChar)
+            : string;
     }
-	
-	function cellNameH(i) {
-		var rest = Math.floor(i / 26) - 1; 
-		var s = (rest > -1 ? cellNameH(rest) : '');
-		return  s+ "ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(i % 26); 
-	}
+
+    function cellNameH(i) {
+        var rest = Math.floor(i / 26) - 1;
+        var s = (rest > -1 ? cellNameH(rest) : '');
+        return s + "ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(i % 26);
+    }
 
     function cellName(colIndex, rowIndex) {
-        return cellNameH(colIndex)+rowIndex;
+        return cellNameH(colIndex) + rowIndex;
     };
 
     function generateCell(cell, column, row, mergeCells) {
-      if (cell.colspan > 1) {
-          var m = { from: cellName(column, row), to: cellName(column + cell.colspan - 1, row) };
-          mergeCells.push(m);
-      }        
-      var s = '<c r="' + cellName(column, row) + '"';
-      if (cell.s) s = s + ' s="' + cell.s + '" ';
-        
-        
-		  var value=cell.v;
-		  if (cell.isstring || isNaN(value)) {
-  			if (cell.isstring || value.charAt(0)!='=') return s + ' t="inlineStr" ><is><t>' + escape(value) + '</t></is></c>';
-			  return s+' ><f>'+value.substring(1)+'</f></c>';
-      }
-		  return s + '><v>' + value + '</v></c>';
+        if (cell.colspan > 1) {
+            var m = { from: cellName(column, row), to: cellName(column + cell.colspan - 1, row) };
+            mergeCells.push(m);
+        }
+        var s = '<c r="' + cellName(column, row) + '"';
+        if (cell.s) s = s + ' s="' + cell.s + '" ';
+
+
+        var value = cell.v;
+        if (cell.isstring || isNaN(value)) {
+            if (cell.isstring || value.charAt(0) != '=') return s + ' t="inlineStr" ><is><t>' + escape(value) + '</t></is></c>';
+            return s + ' ><f>' + value.substring(1) + '</f></c>';
+        }
+        return s + '><v>' + value + '</v></c>';
     }
 
     function generateRow(row, index, mergeCells) {
@@ -557,6 +601,41 @@ $JExcel = {
         }
         return s + "</mergeCells>";
     }
+
+    function generateConditionalFormattings(conditionalFormattings) {
+        if (conditionalFormattings.length === 0) return '';
+
+        var s = '';
+        for (var i = 0; i < conditionalFormattings.length; i++) {
+            var cf = conditionalFormattings[i];
+            s += '<conditionalFormatting sqref="' + cf.range + '">';
+            if (cf.rules && cf.rules.length) {
+                for (var j = 0; j < cf.rules.length; j++) {
+                    var rule = cf.rules[j];
+                    rule.type = rule.type || 'iconSet';
+                    s += '<cfRule type="' + rule.type + '" priority="' + (rule.priority || 1) + '">';
+                    switch (rule.type) {
+                        case 'iconSet':
+                            s += '<iconSet iconSet="' + rule.iconSet + '" showValue="' + (rule.showValue ? '1' : '0') + '">'
+                            if (rule.cfvos && rule.cfvos.length) {
+                                for (var k = 0; k < rule.cfvos.length; k++) {
+                                    var cfvo = rule.cfvos[k];
+                                    s += '<cfvo type="' + (cfvo.type || 'percent') + '" val="' + (cfvo.val || 0) + '"/>';
+                                }
+                            }
+                            s += '</iconSet>'
+                            break;
+                        default:
+                            console.log('myexcel warning: conditionalFormattings rule type="' + rule.type + '" not implemented!');
+                    }
+                    s += '</cfRule>';
+                }
+            }
+            s += '</conditionalFormatting>';
+        }
+        return s;
+    }
+
     function generateRows(rows, mergeCells) {
         var oRows = [];
         for (var index = 0; index < rows.length; index++) {
@@ -605,7 +684,7 @@ $JExcel = {
                 s += '</sheetView>';
             }
         }
-        s += "</sheetViews>"; 
+        s += "</sheetViews>";
         return s;
     }
 
@@ -690,7 +769,7 @@ $JExcel = {
             if (isNaN(column) && isNaN(row)) return s.set(value, style);                                                         // If this is a sheet operation
             if (!isNaN(column)) {                                                                                                // If this is a column operation
                 if (!isNaN(row)) {
-                    var isstring = style && styles.getStyle(style-1).isstring;
+                    var isstring = style && styles.getStyle(style - 1).isstring;
                     return setCell(s.getCell(column, row), value, style, isstring, colspan);                                     // and also a ROW operation the this is a CELL operation
                 }
                 return setColumn(s.getColumn(column), value, style);                                                             // if not we confirm than this is a COLUMN operation
@@ -700,6 +779,10 @@ $JExcel = {
 
         excel.freezePane = function (s, x, y) {
             sheets.get(s).freezePane(x, y);
+        }
+
+        excel.addConditionalFormatting = function (s, column, ruleType, iconSet, showValue, cfvos) {
+            sheets.get(s).addConditionalFormatting(ruleType, column, iconSet, showValue, cfvos);
         }
 
         excel.generate = function (filename) {
@@ -712,9 +795,9 @@ $JExcel = {
             xl.file('_rels/workbook.xml.rels', sheets.toWorkBookRels());                                        // Add WorkBook RELs
             zip.file('[Content_Types].xml', sheets.toContentType());                                            // Add content types
             sheets.fileData(xl);                                                                                // Zip the rest    
-            zip.generateAsync({ type: "blob",mimeType:"application/vnd.ms-excel" }).then(function (content) { saveAs(content, filename); });        // And generate !!!
-       
-	}
+            zip.generateAsync({ type: "blob", mimeType: "application/vnd.ms-excel" }).then(function (content) { saveAs(content, filename); });        // And generate !!!
+
+        }
         return excel;
     }
 })();
